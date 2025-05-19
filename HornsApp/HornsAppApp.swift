44a182fec1dfff041062fd0684a302a6a104e658
Yesferal Cueva <yesferal@gmail.com>
2025-05-19 09:24:05 -0500

    Init xCode iOS project

---
+++ HornsApp/HornsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HornsAppApp.swift
+//  HornsApp
+//
+//  Created by Yesferal Cueva on 5/18/25.
+//
+
+import SwiftUI
+
+@main
+struct HornsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HornsAppTests/HornsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HornsAppTests.swift
+//  HornsAppTests
+//
+//  Created by Yesferal Cueva on 5/18/25.
+//
+
+import Testing
+@testable import HornsApp
+
+struct HornsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
